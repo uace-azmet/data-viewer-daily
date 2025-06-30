8fd83a1a4bfa8be8996fb51bc04a4ce4707ff3ee
Jeremy Weiss <jlweiss@email.arizona.edu>
2025-06-30 15:21:53 -0700

    sidebar, theme, css builds

---
+++ app/R/_global.R
@@ -0,0 +1,58 @@
+# Libraries --------------------
+
+# library(azmetr)
+library(bslib)
+library(bsicons)
+# library(dplyr)
+# library(english)
+library(htmltools)
+library(lubridate)
+# library(plotly)
+library(reactable)
+# library(reshape2)
+library(shiny)
+library(vroom)
+library(tibble)
+
+
+# Files --------------------
+
+# Functions. Loaded automatically at app start if in `R` folder
+#source("./R/fxn_functionName.R", local = TRUE)
+
+# Scripts. Loaded automatically at app start if in `R` folder
+#source("./R/scr_scriptName.R", local = TRUE)
+
+azmetStationMetadata <- 
+  vroom::vroom(
+    file = "aux-files/azmet-station-metadata.csv", 
+    delim = ",", 
+    col_names = TRUE, 
+    show_col_types = FALSE
+  )
+
+
+# Variables --------------------
+
+apiStartDate <- as.Date("2021-01-01")
+
+azmetStationMetadata <- azmetStationMetadata |>
+  dplyr::mutate(
+    end_date = dplyr::if_else(
+      status == "active",
+      lubridate::today(tzone = "America/Phoenix") - 1,
+      end_date
+    )
+  )
+
+# selectedTab <- shiny::reactiveVal(value = "Time Series")
+
+stationGroups <-
+  tibble::tibble(
+    group1 = c("Ft Mohave CA", "Mohave", "Mohave ETo", "Mohave-2", "Parker", "Parker-2"),
+    group2 = c("Roll", "Wellton ETo", "Yuma N.Gila", "Yuma South", "Yuma Valley", "Yuma Valley ETo"),
+    group3 = c("Aguila", "Buckeye", "Harquahala", "Paloma", "Salome", NA),
+    group4 = c("Chino Valley", "Desert Ridge", "Payson", "Phoenix Encanto", "Phoenix Greenway", NA),
+    group5 = c("Coolidge", "Maricopa", "Queen Creek", "Sahuarita", "Test", "Tucson"),
+    group6 = c("Bonita", "Bowie", "Elgin", "Safford", "San Simon", "Willcox Bench")
+  )
--- app/app.R
+++ app/app.R
@@ -1,32 +1,33 @@
-# <App description>
+# Tabular and graphical summaries of daily data to support QA/QC
 
 # Add code for the following
 # 
 # 'azmet-shiny-template.html': <!-- Google tag (gtag.js) -->
 
 
-# Libraries
-# library(azmetr)
-# library(dplyr)
-# library(htmltools)
-# library(lubridate)
-# library(shiny)
-# library(vroom)
-
-# Functions
-#source("./R/fxnABC.R", local = TRUE)
-
-# Scripts
-#source("./R/scr##DEF.R", local = TRUE)
-
-
 # UI --------------------
 
-ui <- htmltools::htmlTemplate(
-  
-  "azmet-shiny-template.html",
-  
-) # htmltools::htmlTemplate()
+ui <- 
+  htmltools::htmlTemplate(
+    "azmet-shiny-template.html",
+    
+    pageSidebar = bslib::page_sidebar(
+      title = NULL,
+
+      sidebar = sidebar, # `scr##_sidebar.R`
+
+      # navsetCardTab, # `scr##_navsetCardTab.R`
+
+      # shiny::htmlOutput(outputId = "figureHelpText"),
+      # shiny::htmlOutput(outputId = "figureFooter"),
+
+      fillable = TRUE,
+      fillable_mobile = FALSE,
+      theme = theme, # `scr##_theme.R`
+      lang = NULL,
+      window_title = NA
+    )
+  )
 
 
 # Server --------------------
@@ -35,10 +36,42 @@ server <- function(input, output, session) {
   
   # Observables -----
   
+  shiny::updateSelectInput(
+    inputId = "azmetStationGroup",
+    label = "AZMet Station Group",
+    choices = c("Apples", "Bananas", "Carrots"), #sort(unique(dataETL()$meta_station_group)),
+    selected = "Carrots" #sort(unique(dataETL()$meta_station_group))[1]
+  )
+  
+  shiny::updateSelectInput(
+    inputId = "stationVariable",
+    label = "Station Variable",
+    choices = c("Apples", "Bananas", "Carrots"),
+      # sort(
+      #   colnames(
+      #     dplyr::select(
+      #       dataETL(), !c(datetime, meta_station_group, meta_station_name)
+      #     )
+      #   )
+      # ),
+    selected = "Bananas"
+      # sort(
+      #   colnames(
+      #     dplyr::select(
+      #       dataETL(), !c(datetime, meta_station_group, meta_station_name)
+      #     )
+      #   )
+      # )[1]
+  )
+  
+  
   # Reactives -----
   
   # Outputs -----
   
+  output$stationGroupsTable <- reactable::renderReactable({
+    stationGroupsTable
+  })
 }
 
 
