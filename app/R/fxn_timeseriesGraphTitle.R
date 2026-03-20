c292635962c38436b136a50a87818efa2c83f173
Jeremy Weiss <jlweiss@email.arizona.edu>
2026-03-20 15:38:07 -0700

    update style of tooltips, section titles, sidebar collapse icon, no-show tabs at initial page loading

--- app/R/fxn_timeseriesGraphTitle.R
+++ app/R/fxn_timeseriesGraphTitle.R
@@ -10,20 +10,22 @@ fxn_timeseriesGraphTitle <- function(startDate, endDate) {
     htmltools::p(
       htmltools::HTML(
         paste0(
-          bsicons::bs_icon("graph-up"), 
-          htmltools::HTML("&nbsp;"),
-          htmltools::HTML("&nbsp;"),
-          toupper(
-            paste0(
-              "Daily data from ", 
-              gsub(" 0", " ", format(startDate, "%B %d, %Y")), 
-              " through ", 
-              gsub(" 0", " ", format(endDate, "%B %d, %Y"))
+          bsicons::bs_icon("graph-up", class = "bolder-icon"), 
+          htmltools::HTML("&nbsp;&nbsp;"),
+          htmltools::HTML(
+            toupper(
+              paste0(
+                "<strong>Daily data from ", 
+                gsub(" 0", " ", format(startDate, "%B %d, %Y")), 
+                " through ", 
+                gsub(" 0", " ", format(endDate, "%B %d, %Y")),
+                "</strong>"
+              )
             )
           )
-        ),
+        )
       ),
-      htmltools::HTML("&nbsp;&nbsp;&nbsp;&nbsp;"),
+      htmltools::HTML("&nbsp;"),
       bslib::tooltip(
         bsicons::bs_icon("info-circle"),
         "Hover over data for variable values and click or tap on legend items to toggle data visibility. Select from the icons to the right of the graph for additional functionality.",
